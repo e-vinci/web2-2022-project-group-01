145495d9e03e0b20fcbaa2caba41bb55597e81c2
Andrei Avram <andrei.avram@student.vinci.be>
2022-11-24 15:45:40 +0100

    2 buttons in HomePage added

--- frontend/src/Components/Pages/HomePage.js
+++ frontend/src/Components/Pages/HomePage.js
@@ -1,6 +1,21 @@
 const HomePage = () => {
   const main = document.querySelector('main');
-  main.innerHTML = 'Deal with the content of your HomePage';
+  const div = document.createElement("div");
+  const div2 = document.createElement("div");
+
+  div.id = 'divHome'
+  div.innerHTML = `       
+  <button type="submit" class="buttonClass Class btn btn-primary  ">
+  Partie Classées
+  </button> `
+  main.appendChild(div);
+  
+  div2.id = 'divHome2'
+  div2.innerHTML = `<button type="submit" class="buttonClass Class btn btn-primary  ">
+  Partie rapide
+  </button> `
+  main.appendChild(div2);
+
 };
 
 export default HomePage;

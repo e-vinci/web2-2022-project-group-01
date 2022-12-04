34ff6a0ee46d2d64c2ced4e832b810011400cf09
NJKatorz <nathanesakwa@gmail.com>
2022-12-04 12:22:37 +0100

    Userpage div

--- frontend/src/Components/Pages/UserPage.js
+++ frontend/src/Components/Pages/UserPage.js
@@ -1,6 +1,20 @@
+import { getAuthenticatedUser } from "../../utils/auths";
+
+const main = document.querySelector("main");
+const divUserPage = document.createElement("div");
+const authenticatedUser = getAuthenticatedUser();
+
 const UserPage = async () => {
-    
+    getUserPage();
     
 };
+
+function getUserPage(){
+
+    divUserPage.id = 'divUser';
+    divUserPage.innerHTML = ` <p> Bienvenue sur votre profil ${authenticatedUser.username} </p> `
+
+    main.appendChild(divUserPage);
+}
 
 export default UserPage;

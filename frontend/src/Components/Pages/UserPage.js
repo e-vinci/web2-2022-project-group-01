72160c9aa68a4611bae819c89900b13e6e2fd153
NJKatorz <nathanesakwa@gmail.com>
2022-12-08 01:20:22 +0100

    UserPage div

--- frontend/src/Components/Pages/UserPage.js
+++ frontend/src/Components/Pages/UserPage.js
@@ -1,9 +1,10 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import { getAuthenticatedUser } from "../../utils/auths";
+// import { getAuthenticatedUser } from "../../utils/auths";
 import { clearPage } from "../../utils/render";
-import profil from "../../img/profil.jpg";
+// import profil from "../../img/profil.jpg";
 
+// `<div><p> Bienvenue sur votre profil ${authenticatedUser.username} </p> </div><br>`
 
 const UserPage = () => {
     clearPage();
@@ -14,40 +15,11 @@ const UserPage = () => {
 function getUserPage() {
     const main = document.querySelector("main");
     const divUserPage = document.createElement("div");
-    const authenticatedUser = getAuthenticatedUser();
-    const divImg = document.createElement("div");
-    const divLevNum = document.createElement("div");
-    const divXp = document.createElement("div");
-    const divBestGame = document.createElement("div");
-    const divAllYourGames = document.createElement("div");
-    const divInfoGames = document.createElement("div");
-    const divLevXp = document.createElement("div");
+    divUserPage.id = "divUser";
 
+    divUserPage.innerHTML = ``;
 
-    divImg.id = 'divImg';
-    divUserPage.id = 'divUser';
-    divLevNum.id = 'divLevNum';
-    divXp.id = 'divXp';
-    divBestGame.id = 'divBestGame';
-    divAllYourGames.id = 'divAllYourGames';
-    divInfoGames.id = 'divInfoGames';
-    divLevXp.id = 'divLevXp';
-
-
-    divImg.innerHTML = `<img src="${profil}">`;
-
-    divUserPage.innerHTML = `<p> Bienvenue sur votre profil ${authenticatedUser.username} </p> `;
-
-    divLevNum.innerHTML = ` <br><div> 
-    <p> Niveau : 13 </p> </div>`
-    divXp.innerHTML = `<p> 16/25 XP </p>`
-
-
-    main.appendChild(divImg);
     main.appendChild(divUserPage);
-
-    main.appendChild(divLevNum);
-    main.appendChild(divXp);
 
 }
 

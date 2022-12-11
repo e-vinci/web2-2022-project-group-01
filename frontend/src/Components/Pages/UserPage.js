2653532878300e75c4d62fc194d6ea528c3b8509
NJKatorz <nathanesakwa@gmail.com>
2022-12-11 22:31:38 +0100

    css xp

--- frontend/src/Components/Pages/UserPage.js
+++ frontend/src/Components/Pages/UserPage.js
@@ -1,3 +1,7 @@
+/* eslint-disable prefer-template */
+/* eslint-disable operator-assignment */
+/* eslint-disable no-extra-semi */
+/* eslint-disable no-plusplus */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/order */
 /* eslint-disable import/named */
@@ -13,6 +17,7 @@ import { clearPage } from "../../utils/render";
 import profil from "../../img/profil.jpg";
 import { getUserInfo, readBestUserScore, readAllUserScore } from "../../models/games";
 import { CircularFluidMeter } from 'fluid-meter';
+import { FluidLayerConfiguration } from "fluid-meter/dist/meters/CircularFluidMeter/Layers/FluidLayer";
 
 const UserPage = () => {
     clearPage();
@@ -29,34 +34,28 @@ async function getUserPage() {
     const divGamesLeft = document.createElement("div");
     const divGamesRight = document.createElement("div");
     divUserPage.id = "divUser";
-   
+
 
     divUserPage.innerHTML = ` 
             
             <div class="divProfil">
                 <img src="${profil}" />
             </div>    
-            
+            <div class="divUser2">  
                 <div class="divProfil2">    
-                    <p> Bienvenue sur votre profil ${user.username} </p>
+                    <p> Welcome to your profil ${user.username} </p>
+            </div>
+                <div class="divLevel">
+                    <div id="divNiv">
+                            <p> Level : 
+                            <br> 
+                            <span id="divNumber">
+                                ${user.level} </p>
+                            </span>    
+                    </div>
+                    <div id="divXp"></div>
                 </div>
-                
-                <br>
-                <br>
-                    <div class="divNiv">
-                        <p> Niveau : ${user.level} </p>
-                    </div>
-                    <div id="divXp">
-                           
-                    </div>
-                   
-         
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>
-        <br>`;
+            </div>`;
 
     // ************************************************************************ //
 
@@ -69,8 +68,9 @@ async function getUserPage() {
 
     const userBestGames = getGamesInfos(userBestScore, true);
 
-    
+
     divGamesLeft.innerHTML = userBestGames;
+    
 
 
     // const userAllScore = await readAllUserScore(user.id_user);
@@ -79,7 +79,8 @@ async function getUserPage() {
     const userAllGames = getGamesInfos(userAllScore, false);
 
     divGamesRight.className = "divGames2";
-    divGamesRight.innerHTML = userAllGames;
+    divGamesRight.innerHTML = userAllGames + `<a> Précédant </a> <a> Suivant </a>`;
+
 
     main.appendChild(divUserPage);
     main.appendChild(divGamesLeft);
@@ -88,12 +89,15 @@ async function getUserPage() {
     // divGames.appendChild(divGamesRight);
     // main.appendChild(divGames);
 
-        // eslint-disable-next-line no-new
-        new CircularFluidMeter(document.querySelector("#divXp"), {
-            initialProgress: 33,
+    // eslint-disable-next-line no-new
 
-            });
-    
+
+    const xpBar = new CircularFluidMeter(document.querySelector("#divXp"), {
+        initialProgress: 33,
+    });
+
+    xpBar.backgroundColor = "#ed2553";
+
 }
 
 
@@ -102,22 +106,46 @@ function getGamesInfos(gameInfos, onlyBestScore) {
     let ligne;
     if (onlyBestScore === true) {
         ligne = "<br> <div id='gridContainer'> <div id=\"divLigne\"> <p> Your Best Games </p> </div>";
-    } else {
+        if (gameInfos.length > 0) {
+            gameInfos.forEach((element) => {
+                ligne += `
+                  <div class="gridItem">
+                      <p><span>Score :</span> ${element.best_score}</p>
+                      <p><span>Xp :</span> ${element.xp}</p> 
+                  `;
+                ligne += '</div>';
+            });
+        } else {
+            ligne += ' <p> NO USER\'S GAMES FOUND</p> ';
+        }
+        ligne += '</div>';
+    }
+
+    else {
         ligne = "<br> <div id='gridContainer'> <div id=\"divLigne\"> <p> All Your Games </p> </div>";
+        if (gameInfos.length > 0) {
+            for (let i = 0; i < 3; i++) {
+                const element = gameInfos[i];
+                ligne += `
+                  <div class="gridItem">
+                      <p><span>Score :</span> ${element.best_score}</p>
+                      <p><span>Xp :</span> ${element.xp}</p> 
+                  `;
+                ligne += '</div>';
+            };
+
+
+        } else {
+            ligne += ' <p> NO USER\'S GAMES FOUND</p> ';
+        }
+        ligne += '</div>';
+        /** 
+            let next = "<a> Précédant </a> <a> Suivant </a>";
+
+            ligne = ligne + next;
+        */
     }
-    if (gameInfos.length > 0) {
-        gameInfos.forEach((element) => {
-            ligne += `
-              <div class="gridItem">
-                  <p><span>Score :</span> ${element.best_score}</p>
-                  <p><span>Xp :</span> ${element.xp}</p> 
-              `;
-            ligne += '</div>';
-        });
-    } else {
-        ligne += ' <p> NO USER\'S GAMES FOUND</p> ';
-    }
-    ligne += '</div>';
+
 
     return ligne;
 
